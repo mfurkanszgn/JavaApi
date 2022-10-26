784c3c45f69f57dc64c3a719ffb9e3a3dd13cacf
mfurkanszgn <furkan.sezgin45@gmail.com>
2022-10-26 15:28:37 +0300

    test brach controle

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Test branch controle");
+        System.out.println("Test branch controle");
     }
-
 
 }
